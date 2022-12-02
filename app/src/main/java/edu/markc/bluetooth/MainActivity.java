d467d5d404ab815f56dddebe3e5827815d24add7
Mark Curran <S00199895@mail.itsligo.ie>
2022-12-02 21:43:10 +0000

    0.1.2 - sending and reading firestore data into charts

--- app/src/main/java/edu/markc/bluetooth/MainActivity.java
+++ app/src/main/java/edu/markc/bluetooth/MainActivity.java
@@ -7,14 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothHidDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ParcelUuid;
@@ -23,36 +22,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.pires.obd.commands.SpeedCommand;
 import com.github.pires.obd.commands.engine.RPMCommand;
-import com.github.pires.obd.commands.protocol.EchoOffCommand;
-import com.github.pires.obd.commands.protocol.LineFeedOffCommand;
-import com.github.pires.obd.commands.protocol.ObdRawCommand;
-import com.github.pires.obd.commands.protocol.SelectProtocolCommand;
-import com.github.pires.obd.commands.protocol.TimeoutCommand;
-import com.github.pires.obd.commands.temperature.AmbientAirTemperatureCommand;
-import com.github.pires.obd.enums.ObdProtocols;
+import com.github.pires.obd.commands.protocol.AvailablePidsCommand_21_40;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
@@ -66,8 +62,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv1 = findViewById(R.id.tV1);
-
+        ArrayList<Map<String, Object>> reads = read();
         BluetoothSocket btSocket = connectToOBD();
+       // ArrayList<Integer> testrpm = new ArrayList<>();
+//        testrpm.add(20);
+//        testrpm.add(30);
+//        testrpm.add(40);
+//        testrpm.add(60);
+//        testrpm.add(20);testrpm.add(20);
+
+//       ArrayList<Map<String, Object>> objsToPush = formatResults(testrpm);
+//
+//        addToFirestore(objsToPush);
+
+      //  ArrayList<Map<String, Object>> wantedReadings = read();
+
 
          if (btSocket.isConnected()) {
              Toast.makeText(MainActivity.this,
@@ -89,36 +98,50 @@ public class MainActivity extends AppCompatActivity {
              }
              try {
                  final Handler handler = new Handler();
+
                  final int delay = 5000;
 
                  InputStream finalInputStream = inputStream;
                  OutputStream finalOutputStream = outputStream;
+
+                 ArrayList<Map<String, Object>> rpmList = new ArrayList<>();
+
                  handler.postDelayed(new Runnable() {
                      @Override
                      public void run() {
+                         //need to check if obd connected
+                         //else
 
-                         /*RPMCommand spd = new RPMCommand();
-                         try {
-                             spd.run(finalInputStream, finalOutputStream);
-                         } catch (IOException e) {
-                             e.printStackTrace();
-                         } catch (InterruptedException e) {
-                             e.printStackTrace();
+                         //work
+                         if (btSocket.isConnected()) {
+                           //  getRunTime(finalInputStream, finalOutputStream);
+                             try {
+                                 Map<String, Object> thisDoc = new HashMap<>();
+                                 LocalDateTime date = LocalDateTime.now();
+
+                                 int rpm = getLiveRPM(finalInputStream, finalOutputStream);
+
+                                 thisDoc.put("type", "RPM");
+                                 thisDoc.put("value", rpm);
+                                 thisDoc.put("datetime", date.toString());
+
+                                 rpmList.add(thisDoc);
+                                 tv1.setText(String.valueOf(rpm));
+                                 //work
+                                 handler.postDelayed(this, delay);
+                             }
+                             catch (NullPointerException e)
+                             {
+                                 addToFirestore(rpmList);
+                             }
                          }
-                         String result = spd.getResult();
-                         String sub = result.substring(8);
-                         int rpm = Integer.parseInt(sub, 16)/4;*/
-                         //work
-                         //int rn = ThreadLocalRandom.current().nextInt(0, 10);
-                         int rpm = getLiveRPM(finalInputStream, finalOutputStream);
-                         tv1.setText(String.valueOf(rpm));
-                         //work
-                         handler.postDelayed(this, delay);
+                         else {
+                        //     ArrayList<Map<String, Object>> formattedValues = formatResults(rpmList);
+                             addToFirestore(rpmList);
+                         }
                      }
                  }, delay);
-                 //DO COMMANDS HERE
 
-                // System.out.print(result);
                /* ObdRawCommand custom = new ObdRawCommand("00"); //command can be any ELM command like "atz" or "0130"
                 custom.run(inputStream, outputStream); //inputs - input stream, outputs - output stream
                 String result = custom.getResult(); // your variable with result*/
@@ -132,6 +155,102 @@ public class MainActivity extends AppCompatActivity {
 
         }
 }
+
+    private ArrayList<Map<String, Object>> read() {
+        //read and query where type is rpm
+        ArrayList<Map<String, Object>> readMaps = new ArrayList<>();
+
+        db = FirebaseFirestore.getInstance();
+       db.collection("data").whereEqualTo("type", "RPM").orderBy("datetime", Query.Direction.ASCENDING)
+               .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                   @Override
+                   public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                       if (task.isSuccessful())
+                       {
+                           for (DocumentSnapshot doc : task.getResult().getDocuments()) {
+                               System.out.println(doc.getData());
+                               readMaps.add(doc.getData());
+
+                           }
+                           makeLineChart(readMaps);
+
+                       } else {
+                           Log.d(TAG, "get failed with ", task.getException());
+                       }
+                       }
+
+               }).addOnFailureListener(new OnFailureListener() {
+                   @Override
+                   public void onFailure(@NonNull Exception e) {
+                       e.printStackTrace();
+                   }
+               });
+
+
+
+        return readMaps;
+    }
+
+    private ArrayList<Map<String, Object>> formatResults(ArrayList<Integer> rpmList) {
+        //foreach
+        //put into a json object array
+        //include date
+        //type
+        //new interface?
+        ArrayList<Map<String, Object>> arrayOfValues = new ArrayList<>();
+        for (int value: rpmList)
+        {
+            //for firestore
+            Map<String, Object> obj = new HashMap<>();
+
+                obj.put("type", "RPM");
+                obj.put("value", value);
+               // obj.put("date", date.toString());
+
+            arrayOfValues.add(obj);
+        }
+        return arrayOfValues;
+        //addToFirestore(arrayOfValues, 1, date, "rpm");
+    }
+
+
+
+    private void addToFirestore(ArrayList<Map<String, Object>> objsToPush) {
+        db = FirebaseFirestore.getInstance();
+
+        for (Map<String, Object> oneDoc : objsToPush) {
+            db.collection("data").add(oneDoc);
+        }
+
+//        Map<String, Object> dateResults = new HashMap<>();
+//        dateResults.put("type", "RPM");
+//        dateResults.put("value", 1000);
+//        dateResults.put("date", "2022-11-30");
+        //db.collection("data").add(dateResults);
+
+//
+//        ArrayList<Map<String, Object>> datawewant = new ArrayList<>();
+//        db.collection("data")
+//                .whereEqualTo("date", "2022-11-30").whereEqualTo("type", "RPM").get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                                datawewant.add(document.getData());
+//                            }
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+//
+//        int val = (Integer)datawewant.get(0).get("value");
+//        System.out.println((String.valueOf(val)));
+
+
+    }
 
     private void addToFirestore() {
         db = FirebaseFirestore.getInstance();
@@ -158,19 +277,32 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    private void makeLineChart() {
-        mChart = (LineChart) findViewById(R.id.lineChart);
+    private void makeLineChart(ArrayList<Map<String, Object>> chartData) {
 
+        System.out.println("chart valeus");
+        System.out.println(chartData);
+        //give this chart times for order and dates
+        //plot against values
+        //display chart
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+
+        int i = 0;
+        for(Map<String, Object> oneDoc : chartData)
+        {
+            yValues.add(new Entry(i, Float.parseFloat(String.valueOf(oneDoc.get("value")))));
+            i++;
+
+        }
+        System.out.println("yvalues");
+        System.out.println(yValues);
+
+
+        mChart = (LineChart) findViewById(R.id.lineChart);
+        mChart.setBackgroundColor(Color.WHITE);
         mChart.setDragEnabled(true);
         mChart.setScaleEnabled(true);
 
-        ArrayList<Entry> yValues = new ArrayList<>();
-
-        yValues.add(new Entry(0, 60f));
-        yValues.add(new Entry(1, 50f));
-        yValues.add(new Entry(2, 20f));
-        yValues.add(new Entry(3, 70f));
-        yValues.add(new Entry(4, 100f));
 
         LineDataSet set1 = new LineDataSet(yValues, "Data");
         set1.setFillAlpha(110);
@@ -254,5 +386,21 @@ public class MainActivity extends AppCompatActivity {
         int rpm = Integer.parseInt(sub, 16)/4;
 
         return  rpm;
+    }
+
+    private void getRunTime(InputStream finalInputStream, OutputStream finalOutputStream) {
+        AvailablePidsCommand_21_40 rTime = new AvailablePidsCommand_21_40();
+
+        try {
+            rTime.run(finalInputStream, finalOutputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String result = rTime.getFormattedResult();
+        Log.d(TAG, "getRunTime: " + result);
     }
 }
